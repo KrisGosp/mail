1ceba447e70e381f845f57a3511c39aba2499f44
Kris Gospodinov <kris060601@gmail.com>
2024-07-10 15:25:41 +0300

    Send Mail + POST response alert

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("#compose").addEventListener("click", compose_email);
 
   // Attach onsubmit event to compose form
-  // document.querySelector("form").onsubmit = submit_email;
+  document.querySelector("#compose-form").onsubmit = submit_email;
 
   // By default, load the inbox
   load_mailbox("inbox");
@@ -38,4 +38,40 @@ function load_mailbox(mailbox) {
   document.querySelector("#emails-view").innerHTML = `<h3>${
     mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
   }</h3>`;
+}
+
+function email_response_alert(message) {
+  let email = document.querySelector("#email-response");
+  if (message === "Email sent successfully.") {
+    email.className = "alert alert-success";
+    email.innerHTML = message;
+  } else {
+    email.className = "alert alert-danger";
+    email.innerHTML = message;
+  }
+  setTimeout(() => {
+    email.className = "d-none";
+    email.innerHTML = "";
+  }, 3000);
+}
+
+function submit_email() {
+  fetch("/emails", {
+    method: "POST",
+    body: JSON.stringify({
+      recipients: document.querySelector("#compose-recipients").value,
+      subject: document.querySelector("#compose-subject").value,
+      body: document.querySelector("#compose-body").value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.message) {
+        email_response_alert(result.message);
+      } else if (result.error) {
+        email_response_alert(result.error);
+      }
+    });
+  load_mailbox("inbox");
+  return false;
 }
